2afc8d553b3b083dcdd4301fb012382499b31518
Dhumin Patel <dhuminpatel@gmail.com>
2024-08-26 16:24:36 -0400

    Delete Exercise 3 - Printing Quotes.py

--- Exercise 3 - Printing Quotes.py
+++
@@ -1,8 +0,0 @@
-quote = input('What is the quote? ')
-said = input('Who said it? ')
-#My own addition (not needed)
-if quote[-1] != '.':
-  quote = quote + '.'
-else: 
-  pass
-print(f'{said} says, "{quote}"')
